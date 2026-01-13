75b48d1c96e85e9b36c17d16eeaf332d87e852d9
hhao99 <hhao99@gmail.com>
2026-01-12 21:15:12 -0600

    helix v1

--- modules/core/editor/helix/default.nix
+++ modules/core/editor/helix/default.nix
@@ -4,6 +4,14 @@
         enable = true;
         settings = {
             theme = "autumn_night_transparent";
+
+            languages.language =[
+                { 
+                    name = "nix";
+                    auto-format = true;
+                    formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+                }
+            ];
             
         };
     };
